3e9ed7ac3cc8fbe0863a2868554721d267e35e2a
Amadeja <obsteter.amadeja@gmail.com>
2024-01-23 05:59:24 +0100

    Create nest.js app, add users module and service
    Created a nest.js app using yarn.
    Added a login route to app controller and changed the default to
    protected.
    Created a User type inside users services, created a User[] storing
    information about mock users, created a method that finds the user based
    on the username provided.
    Added UsersService to the exports inside users.module.ts to make it
    available to other modules.

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Post('login')
+  login(): any {
+    return {}
+  }
+
+  @Get('protected')
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
