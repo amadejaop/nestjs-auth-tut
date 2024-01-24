a67696550f229f60a8116ce9be39ec39c8a423f3
Amadeja <obsteter.amadeja@gmail.com>
2024-01-24 05:28:27 +0100

    Add auth guard
    Created local auth guard to protect the login route using LocalStrategy.
    Added this guard inside the controller to check the login route.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @UseGuards(LocalAuthGuard)
   @Post('login')
-  login(): any {
-    return {}
+  login(@Request() req): any {
+    return req.user;
   }
 
   @Get('protected')
